a892473f05acaf32c01e6723cd081ec19db90a24
sk408 <75577242+sk408@users.noreply.github.com>
2025-03-18 22:56:03 -0700

    Updates

---
+++ assets/BookmarkBorder-BJHwGczU.js
@@ -0,0 +1,1 @@
+import{c as o,j as r}from"./index-B4g7CIIA.js";const c=o(r.jsx("path",{d:"M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2m0 15-5-2.18L7 18V5h10z"}),"BookmarkBorder");export{c as B};
---
+++ assets/School-D-7iYMix.js
@@ -0,0 +1,1 @@
+import{c as o,j as c}from"./index-B4g7CIIA.js";const t=o(c.jsx("path",{d:"M5 13.18v4L12 21l7-3.82v-4L12 17zM12 3 1 9l11 6 9-4.91V17h2V9z"}),"School");export{t as S};
